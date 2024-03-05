2947dc15b586f2b351dd0964dca9a61388bf12ea
Evan Carlson <evanrc1@gmail.com>
2024-03-04 17:56:36 -0800

    Created apollo client setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo';
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <>
       <Navbar />
       <Outlet />
     </>
+    </ApolloProvider>
   );
 }
 
