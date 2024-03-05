ed8cfe087920568ad1c2b85be85b751dae88855a
Evan Carlson <evanrc1@gmail.com>
2024-03-04 18:20:40 -0800

    implemented graphql operations. Defined get_me query and defined mutations

--- client/src/graphQl/mutations.js
+++ client/src/graphQl/mutations.js
@@ -1,1 +1,60 @@
-import { gql }
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            useranme
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $passwordL String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: BookInput!) {
+saveBook(input: $input) {
+    _id
+    username
+    email
+    savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+    }
+  }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+    }
+  }
+}
+`;
