218623de6ab2c65c6d4b99302796022fc55f8d16
Evan Carlson <evanrc1@gmail.com>
2024-03-04 17:51:06 -0800

    added apollo.js file to configure apollo client

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -2,4 +2,30 @@ import { ApolloClient, InMemoryCache, createHttpLink  } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
 // HTTP connection to API
-const httpLink = createHttpLink
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: '/graphql',
+});
+
+// Middleware function that will retrieve the token and attach it to the request.
+const authLink = setContext((_, { headers }) => {
+    // retrieve the authentication token from local storage if it exists
+    const token = localStorage.getItem('id_token');
+    // Return the headers to the context so the httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+// Instantiate the Apollo client with the created httpLink and a new instance of an InMemoryCache
+const client = new ApolloClient({
+    // The authLink middleware is concatenated with the httpLink,
+    // So every request will have the authorization header
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+export default client;
